bc15f0c0c1218c359c5a32722aeb81c517abc4ff
Madhu Perera <37104267+madhuperera@users.noreply.github.com>
2022-03-13 08:21:53 +1300

    Updated with better output

--- Endpoint Analytics/RemovingPowerShellV2.0/Detect-PowerShell_v2.ps1
+++ Endpoint Analytics/RemovingPowerShellV2.0/Detect-PowerShell_v2.ps1
@@ -1,15 +1,22 @@
 [bool] $ExitWithError = $true
 [bool] $ExitWithNoError = $false
 
-if (Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowershellv2)
+$PowerShell_V2 = Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowershellv2
+
+if ($PowerShell_V2)
 {
-    if ((Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowershellv2).State -like "*Enabled*")
+    $State_Of_PShell_v2 = $PowerShell_V2.State
+    if ($State_Of_PShell_v2 -like "*Enabled*")
     {
+        Write-Output "ALERT: PowerShell v2.0 is enabled"
         exit $ExitWithError
     }
+
+    Write-Output "WARNING: PowerShell v2.0 is NOT enabled but in the state $State_Of_PShell_v2"
     exit $ExitWithNoError
 }
 else
 {
+    Write-Output "GOOD: PowerShell v2.0 is not found"
     exit $ExitWithNoError
 }
