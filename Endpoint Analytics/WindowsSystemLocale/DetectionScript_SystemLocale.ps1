cb46979f2d3aab2ad381065ac6192bea9b08b561
Madhu @ EIT <madhu@essentialit.co.nz>
2022-04-19 13:17:04 +1200

    Fixed detection script typo

--- Endpoint Analytics/WindowsSystemLocale/DetectionScript_SystemLocale.ps1
+++ Endpoint Analytics/WindowsSystemLocale/DetectionScript_SystemLocale.ps1
@@ -9,7 +9,6 @@ if ($CurrentSystemLocale -eq $PreferredSystemLocale)
     Write-Output "Preferred Setting has already been applied"
     exit $ExitWithNoError
 }
-}
 else
 {
     Write-Output "Current Setting does not match preferred setting"
