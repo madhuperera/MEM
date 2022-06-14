017497ee5881dcdad76732129848e3bc68cc1aaa
Madhu @ EIT <madhu@essentialit.co.nz>
2022-06-14 15:43:27 +1200

    Update to Cloud Radial Test

--- Intune32_Apps/CloudRadial/Detect_CloudRadial.ps1
+++ Intune32_Apps/CloudRadial/Detect_CloudRadial.ps1
@@ -1,39 +1,14 @@
-param
-(
-    [String] $ServiceName = "CloudRadial"
-)
-[bool] $ExitWithError = $true
-[bool] $ExitWithNoError = $false
-function Test-ServiceExists
+If (Get-Service CloudRadial -ErrorAction SilentlyContinue)
 {
-    param
-    (
-        [String] $F_ServiceName
-    )
-
-    try
-    {
-        $WinService = Get-Service -Name $F_ServiceName -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-        if ($WinService)
-        {
-            return $true
-        }
-        else
-        {
-            return $false
-        }
-    }
-    catch
-    {
-        return $false
-    }
-}
-
-if (Test-ServiceExists -F_ServiceName $ServiceName)
-{
-    exit $ExitWithNoError
+    write-host '<-Start Result->'
+    write-host "STDOUT=CloudRadial Service is already running"
+    write-host '<-End Result->'
+    exit 0
 }
 else
 {
-    exit $ExitWithError
+    write-host '<-Start Result->'
+    write-host "STDOUT=CloudRadial Service is Not Running"
+    write-host '<-End Result->'
+    exit 1
 }
