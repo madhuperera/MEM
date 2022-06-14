41a1b241e4dbde89f40e980b7e2b24ae95e70cd8
Madhu @ EIT <madhu@essentialit.co.nz>
2022-06-14 15:13:50 +1200

    Script to update Defender Tags

---
+++ Intune32_Apps/Defender/UpdateEndpointTags/Update_DefenderTag.ps1
@@ -0,0 +1,43 @@
+param
+    (
+        [String] $SReg_Key_Parent_Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Advanced Threat Protection",
+        [String] $SReg_Key_Name = "DeviceTagging",
+        [String] $SReg_Key_Value_Name = "Group",
+        [String] $SReg_Key_Value_Data = "New_Zealand",
+        [ValidateSet("String","ExpandString","Binary","DWord","MultiString","Qword")] $SReg_Key_Value_Type = "String"
+    )
+
+function Update-RegistryKey
+{
+    param
+    (
+        [String] $Reg_Key_Parent_Path,
+        [String] $Reg_Key_Name,
+        [String] $Reg_Key_Value_Name,
+        [String] $Reg_Key_Value_Data,
+        [ValidateSet("String","ExpandString","Binary","DWord","MultiString","Qword")] $Reg_Key_Value_Type
+    )
+    [bool] $ExitWithError = $true
+    [bool] $ExitWithNoError = $false
+
+    if(!(Test-Path -Path "$Reg_Key_Parent_Path\$Reg_Key_Name" -PathType Container))
+    {
+        New-Item -Path $Reg_Key_Parent_Path -Name $Reg_Key_Name -ItemType Conatiner -Force
+    }
+
+    if ($Reg_Key_Value_Data -and $Reg_Key_Value_Name)
+    {
+        try
+        {
+            New-ItemProperty -Path "$Reg_Key_Parent_Path\$Reg_Key_Name" -Name $Reg_Key_Value_Name -PropertyType String -Value $Reg_Key_Value_Data -Force
+        }
+        catch
+        {
+            exit $ExitWithError
+        }
+        
+    }
+    exit $ExitWithNoError
+}
+
+Update-RegistryKey -Reg_Key_Parent_Path $SReg_Key_Parent_Path -Reg_Key_Name $SReg_Key_Name -Reg_Key_Value_Name $SReg_Key_Value_Name -Reg_Key_Value_Data $SReg_Key_Value_Data -Reg_Key_Value_Type $SReg_Key_Value_Type
