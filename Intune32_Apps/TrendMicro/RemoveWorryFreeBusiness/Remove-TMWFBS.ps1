89315dead2d3b407439935dddb01d42df3bd0be8
Madhu Perera <madhu.perera@concepts.co.nz>
2024-01-12 15:02:24 +1300

    TrendMicro Removal Script

---
+++ Intune32_Apps/TrendMicro/RemoveWorryFreeBusiness/Remove-TMWFBS.ps1
@@ -0,0 +1,75 @@
+param
+(
+    [String] $S_CompanyName = "Sonitlo",
+    [String] $S_ScriptName = "TMWFBS_Removal"
+)
+[bool] $ExitWithError = $true
+[bool] $ExitWithNoError = $false
+function Update-OutputOnExit
+{
+    param
+    (
+        [bool] $F_ExitCode,
+        [String] $F_Message
+    )
+    
+    Write-Host "STATUS=$F_Message" -ErrorAction SilentlyContinue
+
+    if ($F_ExitCode)
+    {
+        exit 1
+    }
+    else
+    {
+        exit 0
+    }
+}
+
+function Start-ScriptLogs
+{
+    param
+    (
+        [String] $F_CompanyName,
+        [String] $F_ScriptName,
+        [String] $F_LogDirectory = "C:\ProgramData\$($F_CompanyName)IntuneManaged\Logs\$F_ScriptName",
+        [String] $F_LogName = "Logs.txt",
+        [String] $F_LogPath = "$F_LogDirectory\$F_LogName"
+    )
+    
+    Start-Transcript -Path $F_LogPath -Force -Append
+}
+
+Start-ScriptLogs -F_CompanyName $S_CompanyName -F_ScriptName $S_ScriptName
+
+# Initiating the Uninstall
+& ".\SCUT.exe" -noinstall
+function IsTrendMicroRunning
+{
+   if (Get-Service | Where-Object {$_.DisplayName -like "Trend Micro*" -or $_.DisplayName -like "Worry-Free Business*"} -ErrorAction SilentlyContinue)
+   {
+        return $true
+   }
+   else
+   {
+        $false
+   }
+
+}
+
+[int] $TotalSleepTime = 0
+while (IsTrendMicroRunning -and $TotalSleepTime -lt 3600 )
+{
+    Start-Sleep -Seconds 60
+    $TotalSleepTime += 60
+}
+
+if (IsTrendMicroRunning)
+{
+    Update-OutputOnExit -F_ExitCode $ExitWithError -F_Message "FAILED"
+}
+else
+{
+    Update-OutputOnExit -F_ExitCode $ExitWithNoError -F_Message "SUCCESS"
+}
+
+Stop-Transcript
