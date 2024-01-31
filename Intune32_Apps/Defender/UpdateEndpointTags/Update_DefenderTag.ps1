eaba1f6a26ab369471cec5e15dd4deabbc6bd421
Madhu Perera <madhu.perera@concepts.co.nz>
2024-01-31 16:07:42 +1300

    Updates

--- Intune32_Apps/Defender/UpdateEndpointTags/Update_DefenderTag.ps1
+++ Intune32_Apps/Defender/UpdateEndpointTags/Update_DefenderTag.ps1
@@ -8,14 +8,13 @@ If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
     Exit
 }
 
-param
-    (
-        [String] $SReg_Key_Parent_Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Advanced Threat Protection",
-        [String] $SReg_Key_Name = "DeviceTagging",
-        [String] $SReg_Key_Value_Name = "Group",
-        [String] $SReg_Key_Value_Data = "New_Zealand",
-        [ValidateSet("String","ExpandString","Binary","DWord","MultiString","Qword")] $SReg_Key_Value_Type = "String"
-    )
+
+[String] $SReg_Key_Parent_Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Advanced Threat Protection"
+[String] $SReg_Key_Name = "DeviceTagging"
+[String] $SReg_Key_Value_Name = "Group"
+[String] $SReg_Key_Value_Data = "New_Zealand"
+[ValidateSet("String","ExpandString","Binary","DWord","MultiString","Qword")] $SReg_Key_Value_Type = "String"
+
 
 function Update-RegistryKey
 {
