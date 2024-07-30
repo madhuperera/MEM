2b639effda319867a990f3675999e996a7b87589
Madhu Perera <37104267+madhuperera@users.noreply.github.com>
2024-07-30 15:51:17 +1200

    Update Detect-FileExSettings.ps1

--- Proactive_Remediations/OpenFileExplorerTo/Detect-FileExSettings.ps1
+++ Proactive_Remediations/OpenFileExplorerTo/Detect-FileExSettings.ps1
@@ -1,1 +1,64 @@
+[bool] $ExitWithError = $true
+[bool] $ExitWithNoError = $false
 
+[String] $SReg_Key_Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+[String] $SReg_Key_Value_Name = "LaunchTo"
+[String] $SReg_Key_Value_Data = "1"
+[ValidateSet("String","ExpandString","Binary","DWord","MultiString","Qword")] $SReg_Key_Value_Type = "DWord"
+
+function Update-OutputOnExit
+{
+    param
+    (
+        [bool] $F_ExitCode,
+        [String] $F_Message
+    )
+    
+    Write-Host "STATUS=$F_Message" -ErrorAction SilentlyContinue
+
+    if ($F_ExitCode)
+    {
+        exit 1
+    }
+    else
+    {
+        exit 0
+    }
+}
+
+Function Get-KeyValueData
+{
+    param
+    (
+        [string]$F_Reg_Key_Path,
+        [string]$F_Reg_Key_Value_Name,
+        [string]$F_Reg_Key_Value_Data
+    )
+
+    $key = Get-Item -Path $F_Reg_Key_Path -ErrorAction SilentlyContinue
+    if ($key -ne $null)
+    {
+        $value = $key.GetValue($F_Reg_Key_Value_Name)
+        if ($value -eq $F_Reg_Key_Value_Data)
+        {
+            return $true
+        }
+    }
+    return $false
+}
+
+try
+{
+    if (Get-KeyValueData -F_Reg_Key_Path $SReg_Key_Path -F_Reg_Key_Value_Name $SReg_Key_Value_Name -F_Reg_Key_Value_Data $SReg_Key_Value_Data -$SReg_Key_Value_Type)
+    {
+        Update-OutputOnExit -F_ExitCode $ExitWithNoError -F_Message "SUCCESS"
+    }
+    else
+    {
+        Update-OutputOnExit -F_ExitCode $ExitWithError -F_Message "FAILED"
+    }
+}
+catch
+{
+    Update-OutputOnExit -F_ExitCode $ExitWithError -F_Message "FAILED"
+}
