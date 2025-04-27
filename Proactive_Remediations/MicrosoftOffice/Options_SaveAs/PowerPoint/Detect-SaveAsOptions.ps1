616906ee1e2173e4819c114b23bc8ff661dd5138
Madhu Perera <madhu.perera@nzmit.nz>
2025-04-27 15:13:41 +1200

    2025-04-27: Save AS Keys for Office Apps

---
+++ Proactive_Remediations/MicrosoftOffice/Options_SaveAs/PowerPoint/Detect-SaveAsOptions.ps1
@@ -0,0 +1,40 @@
+$S_Reg_Key_Path = "HKCU:\Software\Microsoft\Office\16.0\PowerPoint\Options"
+$S_Reg_Key_Value_Name = "defaultformat"
+$S_Reg_Key_Value_Data = 27 # 27 = PowerPoint Presentation (*.pptx)
+
+Function Get-KeyValueData
+{
+    param
+    (
+        [string]$F_Reg_Key_Path,
+        [string]$F_Reg_Key_Value_Name,
+        [string]$F_Reg_Key_Value_Data
+    )
+
+    $key = Get-Item -Path $F_Reg_Key_Path -ErrorAction SilentlyContinue
+    if ($key -ne $null)
+    {
+        $value = $key.GetValue($F_Reg_Key_Value_Name)
+        if ($value -eq $F_Reg_Key_Value_Data)
+        {
+            return $true
+        }
+    }
+    return $false
+}
+
+function Main
+{
+    if (Get-KeyValueData -F_Reg_Key_Path $S_Reg_Key_Path -F_Reg_Key_Value_Name $S_Reg_Key_Value_Name -F_Reg_Key_Value_Data $S_Reg_Key_Value_Data)
+    {
+        Write-Output "SUCCESS: $($S_Reg_Key_Path)\$($S_Reg_Key_Value_Name) is set to $($S_Reg_Key_Value_Data)."
+        exit 0
+    }
+    else
+    {
+        Write-Output "The registry key value is not set to the required value."
+        exit 1
+    }
+}
+
+Main
---
+++ Proactive_Remediations/MicrosoftOffice/Options_SaveAs/Word/Configure-SaveAsOptions.ps1
@@ -0,0 +1,33 @@
+$S_Reg_Key_Path = "HKCU:\Software\Microsoft\Office\16.0\Word\Options"
+$S_Reg_Key_Value_Name = "defaultformat"
+$S_Reg_Key_Value_Data = "" # Default Empty Value = Word Workbook (docx)
+$S_Reg_Key_Value_Type = "String"
+
+Function Set-KeyValueData
+{
+    param
+    (
+        [string]$F_Reg_Key_Path,
+        [string]$F_Reg_Key_Value_Name,
+        [string]$F_Reg_Key_Value_Data,
+        [string]$F_Reg_Key_Value_Type
+    )
+    if (!(Test-Path $F_Reg_Key_Path))
+    {
+        New-Item -Path $F_Reg_Key_Path -Force | Out-Null
+    }
+    New-ItemProperty -Path $F_Reg_Key_Path -Name $F_Reg_Key_Value_Name -Value $F_Reg_Key_Value_Data -PropertyType $F_Reg_Key_Value_Type -Force | Out-Null
+}
+
+try
+{
+    Set-KeyValueData -F_Reg_Key_Path $S_Reg_Key_Path -F_Reg_Key_Value_Name $S_Reg_Key_Value_Name -F_Reg_Key_Value_Data $S_Reg_Key_Value_Data -F_Reg_Key_Value_Type $S_Reg_Key_Value_Type
+    Write-Output "SUCCESS: $($S_Reg_Key_Path)\$($S_Reg_Key_Value_Name) is set to $($S_Reg_Key_Value_Data)."
+    exit 0
+}
+catch
+{
+    Write-Output "ERROR: Failed to set $($S_Reg_Key_Path)\$($S_Reg_Key_Value_Name) to $($S_Reg_Key_Value_Data)."
+    exit 1 
+}
+
