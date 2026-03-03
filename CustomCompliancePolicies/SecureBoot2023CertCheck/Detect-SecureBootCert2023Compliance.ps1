f8cb668545acb1be10ca0d59cdea205cf0935c2e
Madhu Perera <madhu@sonitlo.com>
2026-03-03 13:43:42 +1300

    Updates

--- CustomCompliancePolicies/SecureBoot2023CertCheck/Detect-SecureBootCert2023Compliance.ps1
+++ CustomCompliancePolicies/SecureBoot2023CertCheck/Detect-SecureBootCert2023Compliance.ps1
@@ -53,115 +53,98 @@ function Get-SecureBootCertSubjects {
         [Parameter(Mandatory=$true, HelpMessage="Name of the Secure Boot database to parse")]
         [string]$Database
     )
-    
-    # Get raw UEFI variable - this is line 300 equivalent
-    $db = (Get-SecureBootUEFI -Name $Database).Bytes
-    
-    # Define GUIDs for certificate types
-    $EFI_CERT_X509_GUID = [guid]"a5c059a1-94e4-4aa7-87b5-ab155c2bf072"
-    $EFI_CERT_SHA256_GUID = [guid]"c1c41626-504c-4092-aca9-41f936934328"
-    
-    $signatures = @()
-    
-    # Parse EFI signature database structure (lines 304-333 implementation)
-    for ($o = 0; $o -lt $db.Length; ) {
-        $guid = [Guid][Byte[]]$db[$o..($o+15)]
-        $signatureListSize = [BitConverter]::ToUInt32($db, $o+16)
-        $signatureSize = [BitConverter]::ToUInt32($db, $o+24)
-        $signatureCount = ($signatureListSize - 28) / $signatureSize
-        $so = $o + 28
-        
-        for ($i = 0; $i -lt $signatureCount; $i++) {
-            $signatureOwner = [Guid][Byte[]]$db[$so..($so+15)]
-            
-            # Extract X509 certificates and read Subject
-            if ($guid -eq $EFI_CERT_X509_GUID) {
-                $certBytes = $db[($so+16)..($so+16+$signatureSize-1)]
-                try {
-                    $cert = if ($PSEdition -eq "Core") {
-                        [System.Security.Cryptography.X509Certificates.X509Certificate]::new([Byte[]]$certBytes)
-                    } else {
-                        $c = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-                        $c.Import([Byte[]]$certBytes)
-                        $c
+
+    try {
+        # Get raw UEFI variable - this is line 300 equivalent
+        $db = (Get-SecureBootUEFI -Name $Database).Bytes
+
+        # Define GUIDs for certificate types
+        $EFI_CERT_X509_GUID = [guid]"a5c059a1-94e4-4aa7-87b5-ab155c2bf072"
+        $EFI_CERT_SHA256_GUID = [guid]"c1c41626-504c-4092-aca9-41f936934328"
+
+        $signatures = @()
+
+        # Parse EFI signature database structure (lines 304-333 implementation)
+        for ($o = 0; $o -lt $db.Length; ) {
+            $guid = [Guid][Byte[]]$db[$o..($o+15)]
+            $signatureListSize = [BitConverter]::ToUInt32($db, $o+16)
+            $signatureSize = [BitConverter]::ToUInt32($db, $o+24)
+            $signatureCount = ($signatureListSize - 28) / $signatureSize
+            $so = $o + 28
+
+            for ($i = 0; $i -lt $signatureCount; $i++) {
+                $signatureOwner = [Guid][Byte[]]$db[$so..($so+15)]
+
+                # Extract X509 certificates and read Subject
+                if ($guid -eq $EFI_CERT_X509_GUID) {
+                    $certBytes = $db[($so+16)..($so+16+$signatureSize-1)]
+                    try {
+                        $cert = if ($PSEdition -eq "Core") {
+                            [System.Security.Cryptography.X509Certificates.X509Certificate]::new([Byte[]]$certBytes)
+                        } else {
+                            $c = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+                            $c.Import([Byte[]]$certBytes)
+                            $c
+                        }
+                        # Read cert.Subject property for X509 certificates
+                        $signatures += [PSCustomObject]@{SignatureOwner=$signatureOwner; SignatureSubject=$cert.Subject; Signature=$cert; SignatureType=$guid}
+                    } catch {
+                        $signatures += [PSCustomObject]@{SignatureOwner=$signatureOwner; SignatureSubject="Failed to parse cert"; Signature=$null; SignatureType=$guid}
                     }
-                    # Read cert.Subject property for X509 certificates
-                    $signatures += [PSCustomObject]@{SignatureOwner=$signatureOwner; SignatureSubject=$cert.Subject; Signature=$cert; SignatureType=$guid}
-                } catch {
-                    $signatures += [PSCustomObject]@{SignatureOwner=$signatureOwner; SignatureSubject="Failed to parse cert"; Signature=$null; SignatureType=$guid}
+                } elseif ($guid -eq $EFI_CERT_SHA256_GUID) {
+                    $sha256Hash = ([Byte[]]$db[($so+16)..($so+47)] | ForEach-Object { $_.ToString('X2') }) -join ''
+                    $signatures += [PSCustomObject]@{SignatureOwner=$signatureOwner; Signature=$sha256Hash; SignatureType=$guid}
+                } else {
+                    $unknownData = [Byte[]]$db[($so+16)..($so+16+$signatureSize-1)]
+                    $signatures += [PSCustomObject]@{SignatureOwner=$signatureOwner; SignatureSubject="Unknown signature type"; Signature=$unknownData; SignatureType=$guid}
                 }
-            } elseif ($guid -eq $EFI_CERT_SHA256_GUID) {
-                $sha256Hash = ([Byte[]]$db[($so+16)..($so+47)] | ForEach-Object { $_.ToString('X2') }) -join ''
-                $signatures += [PSCustomObject]@{SignatureOwner=$signatureOwner; Signature=$sha256Hash; SignatureType=$guid}
-            } else { 
-                $unknownData = [Byte[]]$db[($so+16)..($so+16+$signatureSize-1)]
-                $signatures += [PSCustomObject]@{SignatureOwner=$signatureOwner; SignatureSubject="Unknown signature type"; Signature=$unknownData; SignatureType=$guid}
+                $so += $signatureSize
             }
-            $so += $signatureSize
+            $o += $signatureListSize
         }
-        $o += $signatureListSize
+
+        return $signatures
     }
-    
-    return $signatures
+    catch {
+        return $null
+    }
 }
 
 try {
     # Call Get-SecureBootCertSubjects -Database kek (line 224 equivalent)
     $KEKcerts = Get-SecureBootCertSubjects -Database kek
-    
-    # Extract KEK version from certificate subjects
-    $KEKVersion = $null
-    
+    $KEKVersion = "unknown"
+
     if ($KEKcerts) {
         foreach ($cert in $KEKcerts) {
             $subject = $cert.SignatureSubject
-            
-            # Look for "Microsoft Corporation KEK 2K CA XXXX" (or legacy "Microsoft Corporation KEK CA XXXX") and extract the year
-            if ($subject -match 'Microsoft Corporation KEK (?:2K )?CA (\d{4})') {
-                $KEKVersion = $matches[1]
+            if ($subject -like '*Microsoft Corporation KEK 2K CA 2023*') {
+                $KEKVersion = "2023"
                 break
             }
         }
     }
     
-    # If no version found, set as unknown
-    if (-not $KEKVersion) {
-        $KEKVersion = "unknown"
-    }
-    
     # Call Get-SecureBootCertSubjects -Database db to retrieve SecureBootDB
     $DBcerts = Get-SecureBootCertSubjects -Database db
     
-    # Extract DB version from certificate subjects
-    # Look for either "Windows UEFI CA", "Microsoft Corporation UEFI CA", or "Microsoft Windows Production PCA"
-    $DBVersions = @()
-    
+    $DBVersion = "unknown"
+    $DBHas2023 = $false
+
     if ($DBcerts) {
         foreach ($cert in $DBcerts) {
             $subject = $cert.SignatureSubject
-            
-            # Look for "Windows UEFI CA XXXX", "Microsoft Corporation UEFI CA XXXX", or "Microsoft Windows Production PCA XXXX" and extract the year
-            if ($subject -match 'Windows UEFI CA (\d{4})') {
-                $DBVersions += [int]$matches[1]
-            }
-            elseif ($subject -match 'Microsoft Corporation UEFI CA (\d{4})') {
-                $DBVersions += [int]$matches[1]
-            }
-            elseif ($subject -match 'Microsoft Windows Production PCA (\d{4})') {
-                $DBVersions += [int]$matches[1]
+            if (
+                $subject -like '*Windows UEFI CA 2023*' -or
+                $subject -like '*Microsoft UEFI CA 2023*' -or
+                $subject -like '*Microsoft Option ROM UEFI CA 2023*'
+            ) {
+                $DBHas2023 = $true
+                $DBVersion = "2023"
+                break
             }
         }
     }
-    
-    # If versions found, return the oldest (minimum) year, otherwise set as unknown
-    if ($DBVersions.Count -gt 0) {
-        $DBVersion = ($DBVersions | Measure-Object -Minimum).Minimum.ToString()
-    } else {
-        $DBVersion = "unknown"
-    }
-    
-    # Check if DB has Windows UEFI CA 2023 certificate (from Detect-SecureBoot.ps1 line 227)
-    $DBHas2023 = [bool] ($DBcerts | Where-Object { $_.SignatureSubject -match 'Windows UEFI CA 2023' })
     
     # Output as simple JSON with detected values
     $hash = @{
